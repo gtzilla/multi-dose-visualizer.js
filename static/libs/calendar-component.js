1d0077073cf58941c4277a1d8ab1a923ce54b6ce
gtzilla <gregory.tomlinson@gmail.com>
2020-10-04 12:31:20 -0400

    exploring alternate ways to build the calendar that read more cleanly. losing functionality, however.

--- static/libs/calendar-component.js
+++ static/libs/calendar-component.js
@@ -23,6 +23,7 @@ export class CalendarComponent extends React.PureComponent {
     }
     const storedDate = localStorage.getItem('start_date');
     this.state = {
+      viewBounded:'unbounded',
       pattern:pattern || props.pattern,
       activeDate: props.activeDate,
       patternStartDate: storedDate || props.patternStartDate
@@ -69,6 +70,12 @@ export class CalendarComponent extends React.PureComponent {
     }
   }
 
+  handleBoundedViewChange(e) {
+    console.log("changing", e.target.value);
+    this.setState({
+      viewBounded:e.target.value
+    });
+  }
   onChange(e) {
     const splitted = e.target.value.split(',');
     // the 0/1 thing is brittle, but working
@@ -81,35 +88,69 @@ export class CalendarComponent extends React.PureComponent {
     this.setState({ pattern });
     localStorage.setItem('pattern', JSON.stringify(pattern))
   }
-
+  // dosedMonth(currentDate, startDate) {
+  //   const _date = moment(currentDate, [DATE_FORMAT]);
+  //   const startedOn = moment(startDate, [DATE_FORMAT]);
+  //   const startOfMonth = moment(_date).startOf('month');
+  //   const started = startedOn.isAfter(startOfMonth) ? startedOn : startOfMonth;
+  //   const endOfMonth = moment(_date).endOf('month');
+  //   const dayDiff = endOfMonth.diff(started, 'day');
+  //   const accumulate = [];
+  //   for(let i=0; i<dayDiff+1; i++) {
+  //     const active = moment(started).add(i, 'day');
+  //     accumulate.push(active.format(DATE_FORMAT))
+  //   }
+  //   console.log("accumulate", accumulate)
+  //   return accumulate;
+  // }
   render () {
+    // this.dosedMonth(this.state.activeDate, this.state.patternStartDate);
     const today = moment();
     let patternCount = -1;
     const startedOn = moment(this.state.patternStartDate, [DATE_FORMAT]);
     const _date = moment(this.state.activeDate);
+    const monthStart = moment(_date).startOf('month');
     const weekStart = moment(_date).startOf('month').startOf('week');
     const aCalendarDate = startedOn.isBefore(weekStart) ? startedOn : weekStart;
     const endMonthWeek = moment(_date).endOf('month').endOf('week');
     const accumulator = [];
-    while (aCalendarDate.isBefore(endMonthWeek)) {
-      if (aCalendarDate.isSameOrAfter(startedOn)) {
-        patternCount += 1;
-        if (patternCount >= this.state.pattern.length) {
-          patternCount = 0;
+    
+    const daysSince = monthStart.diff(aCalendarDate, 'day');
+    // const modulo = daysSince % this.state.pattern.length
+
+    // I want to be able to calculate
+    // the pattern position for the first of this month
+    console.log("this.state.viewBounded",this.state.viewBounded)
+    if(this.state.viewBounded === 'unbounded') {
+      while (aCalendarDate.isBefore(endMonthWeek)) {
+        if (aCalendarDate.isSameOrAfter(startedOn)) {
+          patternCount += 1;
+          if (patternCount >= this.state.pattern.length) {
+            patternCount = 0;
+          }
         }
-      }
-      if (aCalendarDate.isSameOrAfter(weekStart)) {
-        accumulator.push(
-          el(SingleDayComponent, {
-            today,
-            activeDate:this.state.activeDate,
-            calendarDisplay:aCalendarDate.format(DATE_FORMAT),
-            patternPosition:patternCount,
-            pattern:this.state.pattern
-          }));
-      }
-      aCalendarDate.add(1, 'day');      
+        if (aCalendarDate.isSameOrAfter(weekStart)) {
+          accumulator.push(
+            el(SingleDayComponent, {
+              today,
+              activeDate:this.state.activeDate,
+              calendarDisplay:aCalendarDate.format(DATE_FORMAT),
+              patternPosition:patternCount,
+              pattern:this.state.pattern
+            }));
+        }
+        aCalendarDate.add(1, 'day');      
+      }      
     }
+
+    const dayNames = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday'].map(item=>el('li',{},item));
     const headlineMonthFormat = _date.isSame(today, 'year') ? 'MMMM' : 'MMMM YYYY'
     return (
       el(React.Fragment, {},
@@ -124,6 +165,17 @@ export class CalendarComponent extends React.PureComponent {
             className:'visualize-input-label'
           },
             'Visualizing',
+          el('form', {},
+            el('select', {
+                value:this.state.viewBounded,
+                onChange:this.handleBoundedViewChange.bind(this),
+              },
+              el('option', {
+                value:'unbounded'
+              }, 'Unbounded'),
+              el('option', {
+                value:'bounded'
+              }, 'Bounded'))),
           el('input', {
             type:'text',
             onChange:this.onChange.bind(this),
@@ -150,6 +202,10 @@ export class CalendarComponent extends React.PureComponent {
               className:'calendar-next-month-arrow',
               onClick:this.onClickNext.bind(this)(nextMonth(_date))
             }, 'next')))),
+        el('div', {
+          className:'calendar-component-header'
+        },
+          el('ul', {}, ...dayNames)),
         el('div', {
           className: 'calendar-component-box'
         }, ...accumulator)));
